21aa1e79e237af33507a4baec2ced9783185c769
gunturkh <guntur.kh@gmail.com>
2018-08-02 23:41:15 +0700

    Creating index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require ("express")
+const bodyParser = require ("body-parser")
+
+const controllers = require("./controllers")
+const todos = require("./todos")
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", controllers.displayInfo)
+app.post("/", controllers.displayName)
+
+app.get("/todos", todos.display)
+app.post("/todos", todos.add)
+
+app.get("/todos:id",todos.displayId)
+app.post("/todos:id",todos.add)
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on Port ${port}`);
+})
+
