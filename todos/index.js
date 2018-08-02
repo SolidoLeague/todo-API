9d428c94f91c73c180cc738b8b4f9ae04cc58109
gunturkh <guntur.kh@gmail.com>
2018-08-02 23:41:28 +0700

    Creating index.js in todos folder

---
+++ todos/index.js
@@ -0,0 +1,31 @@
+const fs = require("fs")
+
+const todosJSON = "./data/todos.json"
+const TODOS = JSON.parse(fs.readFileSync(todosJSON, 'utf8'))
+
+const todos = {
+    display: (req, res) => {
+        res.status(200).send(TODOS);
+    },
+
+    displayId: (req,res) => {
+        res.status(200).send(req.params)
+    },
+    
+    add: (req, res) => {
+        const data = {
+            id: TODOS.length,
+            text: req.body.text
+        }
+
+        TODOS.push(data)
+
+        const todosString = JSON.stringify(TODOS, null, 2)
+
+        fs.writeFileSync(todosJSON, todosString, 'utf8');
+
+        res.status(201).send(data)
+    }
+};
+
+module.exports = todos;
