dc040bc9d68fa38c0735966eac002677aedeff66
gunturkh <guntur.kh@gmail.com>
2018-08-03 14:18:31 +0700

    Add method to get, post, delete and update todos by id

--- todos/index.js
+++ todos/index.js
@@ -4,15 +4,22 @@ const todosJSON = "./data/todos.json"
 const TODOS = JSON.parse(fs.readFileSync(todosJSON, 'utf8'))
 
 const todos = {
-    display: (req, res) => {
+    get: (req, res) => {
         res.status(200).send(TODOS);
     },
 
-    selectId: (req, res) => {
-        res.status(200).send(TODOS[req.params.id])
-        console.log(req.params.id);
-        
-        
+    getOnebyId: (req, res) => {
+        const data = TODOS.find(item => {
+            return item.id === Number(req.params.id)
+        })
+        if (data) {
+            res.status(200).send(data)
+        }
+        else {
+            res.status(400).send({
+                message: "Id is not found"
+            })
+        }
     },
 
     add: (req, res) => {
@@ -20,15 +27,65 @@ const todos = {
             id: TODOS.length,
             text: req.body.text
         }
+        console.log(req.body.text)
 
         TODOS.push(data)
 
-        const todosString = JSON.stringify(TODOS, null, 2)
+        const todosString = JSON.stringify(TODOS, null, 1)
 
         fs.writeFileSync(todosJSON, todosString, 'utf8');
 
         res.status(201).send(data)
     },
+
+    saveTodo: (req, res) => {
+        const data = {
+            id: req.body.id,
+            text: req.body.text
+        }
+        res.status(200).send(data)
+    },
+
+    deleteTodos: (req, res) => {
+        let data = TODOS
+        data = []
+        res.status(200).send(data)
+    },
+
+    deleteTodobyId: (req, res) => {
+        const data = TODOS.find(item => {
+            return item.id === Number(req.params.id)
+        })
+        if (data) {
+
+            delete data['text']
+            delete data['id']
+            res.status(200).send(data)
+        }
+        else {
+            res.status(404).send({ message: "Id not found" })
+        }
+    },
+
+    updateTodobyId: (req, res) => {
+        const data = TODOS.find(item => {
+            return item.id === Number(req.params.id)
+        })
+
+        if (data) {
+            const updateData = {
+                id: req.body.id,
+                text: req.body.text
+            }
+
+            res.status(200).send(updateData)
+        }
+        else {
+            res.status(404).send({
+                message: "Id not found"
+            })
+        }
+    }
 };
 
 module.exports = todos;
