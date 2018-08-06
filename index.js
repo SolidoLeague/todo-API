0247b7be45352ccc18004c79700a73523d131a86
Maya Sopiah Lubis <mayasopie275@gmail.com>
2018-08-06 10:37:35 +0700

    Add NPM package CORS use function

--- index.js
+++ index.js
@@ -9,13 +9,19 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/todos", todos.get);
-app.post("/todos", todos.add);
-
 app.get("/todos/:search",todos.getOnebyCharacters);
+app.post("/todos", todos.add);
+app.put("/todos/:id", todos.updateTodoById);
+app.delete("/todos/:id",todos.deleteTodoById)
 
-// app.put("/todos/:id", todos.updateTodoById);
-// app.delete("/todos/:id",todos.deleteTodoById)
+
 
 
 app.listen(port, () => {
